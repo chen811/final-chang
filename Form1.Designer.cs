e47de2950cf9ae11ec066c1c3e3a9fb94038a255
user <user@DESKTOP-7L4BJMM>
2021-01-14 11:53:49 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿using System;
+
+namespace Guest_Big_Small
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnsmall = new System.Windows.Forms.Button();
+            this.btnred = new System.Windows.Forms.Button();
+            this.btnblack = new System.Windows.Forms.Button();
+            this.btnbig = new System.Windows.Forms.Button();
+            this.btnend = new System.Windows.Forms.Button();
+            this.infolabel = new System.Windows.Forms.Label();
+            this.lbwinlose = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbmoney = new System.Windows.Forms.Label();
+            this.tbmoney = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbtoptext = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnDeal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(255, 64);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(361, 416);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnsmall
+            // 
+            this.btnsmall.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnsmall.Location = new System.Drawing.Point(125, 64);
+            this.btnsmall.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnsmall.Name = "btnsmall";
+            this.btnsmall.Size = new System.Drawing.Size(113, 50);
+            this.btnsmall.TabIndex = 16;
+            this.btnsmall.Text = "小";
+            this.btnsmall.UseVisualStyleBackColor = true;
+            this.btnsmall.Click += new System.EventHandler(this.btnsmall_Click);
+            // 
+            // btnred
+            // 
+            this.btnred.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnred.Location = new System.Drawing.Point(125, 131);
+            this.btnred.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnred.Name = "btnred";
+            this.btnred.Size = new System.Drawing.Size(113, 50);
+            this.btnred.TabIndex = 17;
+            this.btnred.Text = "紅";
+            this.btnred.UseVisualStyleBackColor = true;
+            this.btnred.Click += new System.EventHandler(this.btnred_Click);
+            // 
+            // btnblack
+            // 
+            this.btnblack.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnblack.Location = new System.Drawing.Point(6, 131);
+            this.btnblack.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnblack.Name = "btnblack";
+            this.btnblack.Size = new System.Drawing.Size(113, 50);
+            this.btnblack.TabIndex = 18;
+            this.btnblack.Text = "黑";
+            this.btnblack.UseVisualStyleBackColor = true;
+            this.btnblack.Click += new System.EventHandler(this.btnblack_Click);
+            // 
+            // btnbig
+            // 
+            this.btnbig.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnbig.Location = new System.Drawing.Point(6, 64);
+            this.btnbig.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnbig.Name = "btnbig";
+            this.btnbig.Size = new System.Drawing.Size(113, 50);
+            this.btnbig.TabIndex = 20;
+            this.btnbig.Text = "大";
+            this.btnbig.UseVisualStyleBackColor = true;
+            this.btnbig.Click += new System.EventHandler(this.btnbig_Click);
+            // 
+            // btnend
+            // 
+            this.btnend.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnend.Location = new System.Drawing.Point(125, 198);
+            this.btnend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnend.Name = "btnend";
+            this.btnend.Size = new System.Drawing.Size(113, 50);
+            this.btnend.TabIndex = 21;
+            this.btnend.Text = "結束遊戲";
+            this.btnend.UseVisualStyleBackColor = true;
+            this.btnend.Click += new System.EventHandler(this.btnend_Click);
+            // 
+            // infolabel
+            // 
+            this.infolabel.AutoSize = true;
+            this.infolabel.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.infolabel.Location = new System.Drawing.Point(669, 109);
+            this.infolabel.Name = "infolabel";
+            this.infolabel.Size = new System.Drawing.Size(0, 30);
+            this.infolabel.TabIndex = 22;
+            // 
+            // lbwinlose
+            // 
+            this.lbwinlose.AutoSize = true;
+            this.lbwinlose.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbwinlose.Location = new System.Drawing.Point(669, 151);
+            this.lbwinlose.Name = "lbwinlose";
+            this.lbwinlose.Size = new System.Drawing.Size(0, 30);
+            this.lbwinlose.TabIndex = 23;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(900, 493);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 33);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "你的財產";
+            // 
+            // lbmoney
+            // 
+            this.lbmoney.AutoSize = true;
+            this.lbmoney.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbmoney.ForeColor = System.Drawing.Color.Blue;
+            this.lbmoney.Location = new System.Drawing.Point(943, 531);
+            this.lbmoney.Name = "lbmoney";
+            this.lbmoney.Size = new System.Drawing.Size(55, 30);
+            this.lbmoney.TabIndex = 25;
+            this.lbmoney.Text = "100";
+            // 
+            // tbmoney
+            // 
+            this.tbmoney.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbmoney.Location = new System.Drawing.Point(650, 64);
+            this.tbmoney.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbmoney.Name = "tbmoney";
+            this.tbmoney.Size = new System.Drawing.Size(100, 43);
+            this.tbmoney.TabIndex = 26;
+            this.tbmoney.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(634, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 33);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "押注金額";
+            // 
+            // lbtoptext
+            // 
+            this.lbtoptext.AutoSize = true;
+            this.lbtoptext.Font = new System.Drawing.Font("新細明體", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lbtoptext.Location = new System.Drawing.Point(228, 13);
+            this.lbtoptext.Name = "lbtoptext";
+            this.lbtoptext.Size = new System.Drawing.Size(321, 28);
+            this.lbtoptext.TabIndex = 27;
+            this.lbtoptext.Text = "2~7小牌 9~A大牌 8點通殺";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(811, 575);
+            this.tabControl1.TabIndex = 28;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lbtoptext);
+            this.tabPage1.Controls.Add(this.tbmoney);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.lbwinlose);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.btnend);
+            this.tabPage1.Controls.Add(this.infolabel);
+            this.tabPage1.Controls.Add(this.btnDeal);
+            this.tabPage1.Controls.Add(this.btnbig);
+            this.tabPage1.Controls.Add(this.btnred);
+            this.tabPage1.Controls.Add(this.btnblack);
+            this.tabPage1.Controls.Add(this.btnsmall);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(803, 546);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "阿罵的賭場";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(871, 28);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(189, 238);
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(906, 272);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 52);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "換個對手";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnDeal
+            // 
+            this.btnDeal.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDeal.Location = new System.Drawing.Point(6, 198);
+            this.btnDeal.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(113, 50);
+            this.btnDeal.TabIndex = 19;
+            this.btnDeal.Text = "發牌";
+            this.btnDeal.UseVisualStyleBackColor = true;
+            this.btnDeal.Click += new System.EventHandler(this.btnDeal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 578);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.lbmoney);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "押大小或是牌色";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+       
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnsmall;
+        private System.Windows.Forms.Button btnred;
+        private System.Windows.Forms.Button btnblack;
+        private System.Windows.Forms.Button btnbig;
+        private System.Windows.Forms.Button btnend;
+        private System.Windows.Forms.Label infolabel;
+        private System.Windows.Forms.Label lbwinlose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbmoney;
+        private System.Windows.Forms.TextBox tbmoney;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbtoptext;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnDeal;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Guest_Big_Small
+{
+    public partial class Form1 : Form
+    {
+
+
+        List<Image> head = new List<Image>();
+        List<Image> risk = new List<Image>();
+
+        public class resp
+        {
+            public List<img> data { get; set; }
+        }
+
+        public class img
+        {
+            public string link { get; set; }
+        }
+
+        private resp GetImages(string albumHash, string clientId)
+        {
+            resp result = null;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://api.imgur.com/3/album/{albumHash}/images");
+                //Add Header 
+                WebHeaderCollection webHeaderCollection = request.Headers;
+                webHeaderCollection.Add("Authorization", $"Client-ID {clientId}");
+
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                string json = readStream.ReadToEnd();
+
+                result = JsonConvert.DeserializeObject<resp>(json);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+
+            }
+            return result;
+        }
+
+        private Image GetImageFromUrl(string url)
+        {
+            Image result;
+            try
+            {
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream receiveStream = response.GetResponseStream();
+                result = System.Drawing.Image.FromStream(receiveStream);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp.ToString());
+                throw;
+            }
+            return result;
+        }
+
+        string[] d = new string[54];  
+        static int[] a = new int[52];      
+
+        int point, flower, bigsmall;
+
+        public enum guessCard { red,black,big,samll };
+
+        bool red;    
+        bool gameover;  
+
+        string topText = "A為大牌，2~7小牌，9以上大牌，8大小通吃";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gameover = false;
+            EnableButton(false);
+            infolabel.Text = string.Empty;
+
+            string picPath = GetDirectory("pic");      
+            for (int i = 0; i <= 52; i++)
+                d[i] = picPath + @"\g" + i.ToString() + ".jpg";
+        }
+
+        private string GetDirectory(string name)
+        {
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Application.StartupPath);
+            string path = (dir.Parent).Parent.FullName;
+            return path + string.Format(@"\{0}\", name);
+        }
+
+        private void btnDeal_Click(object sender, EventArgs e)
+        {
+            int i, rnum, temp;
+
+            EnableButton(true);
+            pictureBox1.Image = Image.FromFile(@d[52]);
+
+                Random ran = new Random();
+                for (i = 0; i < 52; i++)
+                {
+                    a[i] = i;
+                }
+                for (i = 0; i < 52; i++)
+                {
+                    rnum = ran.Next(51);
+                    temp = a[i];
+                    a[i] = a[rnum];
+                    a[rnum] = temp;
+                }                               
+            
+
+            if(gameover)
+            {
+                lbtoptext.Text = topText;
+                btnDeal.Text = "發牌";
+                tbmoney.Text = "10";
+                lbmoney.Text = "100";
+                gameover = false;
+            }
+        }
+
+        enum state { red=1, black=2, big=4, small=8, both=16 }
+
+        public void opencard(guessCard guess)
+        {
+            bool bingo = false;
+            int playerguess = (int)guess;
+            int playermoney;
+            if (!Int32.TryParse(lbmoney.Text, out playermoney))
+            {
+                playermoney = 0;
+            }
+            int betMoney;
+            if (!Int32.TryParse(tbmoney.Text, out betMoney))
+            {
+                betMoney = 0;
+                MessageBox.Show("金額有在小數點逆?");
+                return;
+            }
+
+            if (betMoney > 0 && betMoney <= playermoney) 
+            {
+                EnableButton(false); 
+
+                pictureBox1.Image = Image.FromFile(@d[a[0]]);
+                int cardRate = -1;
+                red = true;
+                infolabel.Text = "";
+
+                flower = (byte)a[0] % 4;       
+                point = a[0] / 4;              
+         
+                if (flower == 3 || flower == 0) 
+                {
+                    red = false;
+                    infolabel.Text = "黑色,";
+                    if (playerguess == 1)
+                        bingo = true;
+                }
+                else
+                {
+                    infolabel.Text = "紅色,";
+                    if (playerguess == 0)
+                        bingo = true;
+                }
+
+                if (point == 7)
+                {
+                    bigsmall = 0;
+                    infolabel.Text += "通殺";
+                    if (playerguess > 1)
+                        bingo = true;
+                }
+                else if (point < 7 && point != 0)
+                {
+                    bigsmall = 1;
+                    infolabel.Text += "小";
+                    if (playerguess == 3)
+                        bingo = true;
+                }
+                else
+                {
+                    bigsmall = 2;
+                    infolabel.Text += "大";
+                    if (playerguess == 2)
+                        bingo = true;
+                }
+
+                lbwinlose.Text = bingo ? "你贏了" : "你輸了";
+                if (bingo)
+                {
+                    cardRate = 1;
+                }
+                playermoney += betMoney * cardRate;
+                lbmoney.Text = playermoney.ToString();
+
+                if (playermoney <= 0)
+                {
+                    btnDeal.Text = "重新開始";
+                    lbtoptext.Text = "你破產了，窮B";
+                    gameover = true;
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("請輸入大於0並且小於等於剩餘籌碼的金額。");
+            }
+        }
+
+        private void btnbig_Click(object sender, EventArgs e)
+        {    
+            opencard(guessCard.big);
+        }
+
+        private void btnsmall_Click(object sender, EventArgs e)
+        {           
+            opencard(guessCard.samll);
+        }
+
+        private void btnblack_Click(object sender, EventArgs e)
+        {
+            opencard(guessCard.black);
+        }       
+
+        private void btnred_Click(object sender, EventArgs e)
+        {         
+            opencard(guessCard.red);
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnableButton(bool isEnable)
+        {
+            btnbig.Enabled = isEnable;
+            btnsmall.Enabled = isEnable;
+            btnred.Enabled = isEnable;
+            btnblack.Enabled = isEnable;
+            btnDeal.Enabled = !isEnable;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //更換敵人
+            var imgurData = GetImages("RhhOaIU", "5add18c101a7102");
+
+            if (imgurData == null)
+            {
+                return;
+            }
+            int randomNum = new Random().Next(8);
+            Image image = GetImageFromUrl(imgurData.data[randomNum].link);
+            pictureBox3.Image = image;
+            if (image == null)
+            {
+                return;
+            }
+
+            //json --> object 
+            Console.WriteLine(imgurData);
+        }
+        
+
+
+
+        private void btnend_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("你確定要結束遊戲嗎?","結束遊戲", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                Close();
+            }  
+        }
+
+        private class JsonConvert
+        {
+            internal static T DeserializeObject<T>(string json)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
